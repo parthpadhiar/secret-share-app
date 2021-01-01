9bc43b4991d9b0d1807cb7533f9f9da00e07ade1
Parth Padhiar <parthpadhiar99@gmail.com>
2021-01-01 07:45:36 +0530

    Feat: CRUD for secrets of User

--- controllers/secrets.controller.js
+++ controllers/secrets.controller.js
@@ -1,6 +1,36 @@
 const Secret = require("../models/secrets.model.js");
 const log = require("../constants/bunyan.constant");
 
+exports.showSecret = (req, res) => {
+  try {
+    const user_id = req.user.user_id;
+    Secret.findOne({ user_id: user_id }, (err, foundUserSecret) => {
+      if (foundUserSecret !== null) {
+        Secret.find({ user_id: user_id }, (err, data) => {
+          if (!err) {
+            log.info(`User ${user_id}'s Secrets: ${data}`);
+            res.status(200).json({
+              succes: true,
+              message: `Secrets of user_id: ${user_id}`,
+              user: data,
+            });
+          } else {
+            log.error(`Error in showing Secrets: ${err}`);
+          }
+        });
+      } else {
+        log.error(`Secret for this user Does not Exists: ${err}`);
+        res.status(400).json({
+          succes: false,
+          message: "Secret for this user Does not Exists",
+        });
+      }
+    });
+  } catch (error) {
+    log.error(`Error in showSecret fuction: ${error}`);
+  }
+};
+
 exports.addSecret = (req, res) => {
   try {
     const secret = req.body.secret;
@@ -30,27 +60,71 @@ exports.editSecret = (req, res) => {
     const user_id = req.user.user_id;
     const secret_id = req.params.secret_id;
     const secret = req.body.secret;
-    Secret.findOneAndUpdate(
-      { user_id: user_id, secret_id: secret_id },
-      {
-        $set: {
-          secret: secret,
-        },
-      },
-      { new: true },
-      (err, data) => {
-        if (err) {
-          log.error(`Error in Updating Secret: ${err}`);
-        }
-        log.info(`Successfully Update Secret: ${data}`);
-        res.status(200).json({
-          succes: true,
-          message: `Successfully Updated Secret of secret_id: ${secret_id}`,
-          user: data,
+    Secret.findOne({ secret_id: secret_id }, (err, foundSecret) => {
+      if (foundSecret !== null) {
+        Secret.findOneAndUpdate(
+          { user_id: user_id, secret_id: secret_id },
+          {
+            $set: {
+              secret: secret,
+            },
+          },
+          { new: true },
+          (err, data) => {
+            if (err) {
+              log.error(`Error in Updating Secret`);
+            }
+            log.info(`Successfully Update Secret: ${data}`);
+            res.status(200).json({
+              succes: true,
+              message: `Successfully Updated Secret of secret_id: ${secret_id}`,
+              user: data,
+            });
+          }
+        );
+      } else {
+        log.error(`Secret Does not Exists: ${err}`);
+        res.status(400).json({
+          succes: false,
+          message: "Secret Does not exist",
         });
       }
-    );
+    });
   } catch (error) {
     log.error(`Error in editSecret Fuction: ${error}`);
+  }
+};
+
+exports.deleteSecret = (req, res) => {
+  try {
+    const user_id = req.user.user_id;
+    const secret_id = req.params.secret_id;
+    Secret.findOne({ secret_id: secret_id }, (err, foundSecret) => {
+      if (foundSecret !== null) {
+        Secret.findOneAndDelete(
+          { user_id: user_id, secret_id: secret_id },
+          (err, data) => {
+            if (err) {
+              log.error(`Error in Deleting Secret: ${err}`);
+            } else {
+              log.info(`Successfully Delete Secret: ${data}`);
+              res.status(200).json({
+                succes: true,
+                message: `Successfully Deleted Secret of secret_id: ${secret_id}`,
+                user: data,
+              });
+            }
+          }
+        );
+      } else {
+        log.error(`Secret Does not Exists: ${err}`);
+        res.status(400).json({
+          succes: false,
+          message: "Secret Does not exist",
+        });
+      }
+    });
+  } catch (error) {
+    log.error(`Error in deleteSecret Fuction: ${error}`);
   }
 };
--- routes/secrets.route.js
+++ routes/secrets.route.js
@@ -3,18 +3,39 @@ const routes = express.Router();
 const { auth } = require("../middlewares/auth");
 const secretController = require("../controllers/secrets.controller");
 
+const PATH = {
+  SHOW_ALL_SECRETS: "/api/secret/all",
+  ADD_SECRET: "/api/secret/add",
+  EDIT_SECRET: "/api/secret/edit/:secret_id",
+  DELETE_SECRET: "/api/secret/delete/:secret_id",
+};
+
 /**
  * {GET}
- * localhost:3000/
+ * localhost:3000/api/secret/all
+ * Logged In users secret only
+ */
+routes.get(PATH.SHOW_ALL_SECRETS, auth, secretController.showSecret)
+
+/**
+ * {POST}
+ * localhost:3000/api/secret/add
  * Welcome API
  */
-routes.post("/api/secret", auth,secretController.addSecret);
+routes.post(PATH.ADD_SECRET, auth, secretController.addSecret);
 
 /**
  * {PUT}
- * localhost:3000/api/secret/:secret_id
- * edit Secret 
+ * localhost:3000/api/secret/edit/:secret_id
+ * edit Secret
  */
-routes.put("/api/secret/:secret_id",auth, secretController.editSecret)
+routes.put(PATH.EDIT_SECRET, auth, secretController.editSecret);
+
+/**
+ * {DELETE}
+ *  localhost:3000/api/secret/delete/:secret_id
+ * delete Secret
+ */
+routes.delete(PATH.DELETE_SECRET, auth, secretController.deleteSecret);
 
 module.exports = routes;
